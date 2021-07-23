50e278c0c3731e3f9c30f2248f9b2598d7e1122d
1806134 <87552699+1806134@users.noreply.github.com>
2021-07-23 11:45:55 +0900

    最終課題

---
+++ JS/jQuery.js
@@ -0,0 +1,30 @@
+function quis1() {
+
+    $('#hint1').toggle();
+    
+}
+function quis2() {
+
+    $('#hint2').toggle();
+    
+}
+function quis3() {
+
+    $('#hint3').toggle();
+    
+}
+function quis4() {
+
+    $('#hint4').toggle();
+    
+}
+function quis5() {
+
+    $('#hint5').toggle();
+    
+}
+
+function comentary() {
+
+    $('#comment').toggle();
+}
---
+++ JS/java.js
@@ -0,0 +1,60 @@
+function Q1(Answer) {
+    var message = "";
+
+    if (Answer == 1) {
+        Answercheck = '正解！';
+    } else {
+        Answercheck = '不正解...正解はB'
+    }
+    document.getElementById('resultMsg').innerHTML =　'この問題、' +Answercheck;
+    
+}
+
+function Q2(Answer) {
+    var message = ""
+
+    if (Answer == 1) {
+        Answercheck = '正解！';
+    } else {
+        Answercheck = '不正解...正解はC'
+    }
+    document.getElementById('resultMsg2').innerHTML =　'この問題、' +Answercheck;
+    
+}
+
+function Q3(Answer) {
+    var message = ""
+    
+    if (Answer == 1) {
+        Answercheck = '正解！';
+    } else {
+        Answercheck = '不正解...正解はB'
+    }
+    document.getElementById('resultMsg3').innerHTML =　'この問題、' +Answercheck;
+    
+}
+
+function Q4(Answer) {
+    var message = ""
+    
+    if (Answer == 1) {
+        Answercheck = '正解！';
+    } else {
+        Answercheck = '不正解...正解はA'
+    }
+    document.getElementById('resultMsg4').innerHTML =　'この問題、' +Answercheck;
+    
+}
+
+
+function Q5(Answer) {
+    var message = ""
+    
+    if (Answer == 1) {
+        Answercheck = '正解！';
+    } else {
+        Answercheck = '不正解...正解はA'
+    }
+    document.getElementById('resultMsg5').innerHTML =　'この問題、' +Answercheck;
+    
+}
