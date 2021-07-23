28ca36c58a19b6a45563929ef3a801aa032780ad
1806134 <87552699+1806134@users.noreply.github.com>
2021-07-23 10:53:01 +0900

    最終課題

---
+++ JS/jQuery.js
@@ -0,0 +1,35 @@
+function quis1() {
+
+    $('#hint1').toggle();
+    
+}
+function quis2() {
+
+    $('#hint2').toggle();
+    
+}
+function quis3() {
+
+    $('#hint3').toggle();
+    
+}
+function quis4() {
+
+    $('#hint4').toggle();
+    
+}
+function quis5() {
+
+    $('#hint5').toggle();
+    
+}
+function quis6() {
+
+    $('#hint6').toggle();
+
+}
+
+function comentary() {
+
+    $('#comment').toggle();
+}
